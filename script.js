3228645eb7dca4d6976e4035f8f24279b58b2ca3
AK24dotcom <112376593+AK24dotcom@users.noreply.github.com>
2022-10-10 11:56:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -70,8 +70,8 @@ function resetcar() {
 }
 function display1() {
 
-    var dicg = [{ name: "Neil", r_No: "20cs1037", dob: "19-05-1922" },
-    { name: "david", r_No: "20cs2041", dob: "19-06-2003" }];
+    var dicg = [{ name: "Akumlong", r_No: "20cs1137", dob: "19-05-1922" },
+    { name: "Imsong", r_No: "20cs1137.", dob: "19-06-2003" }];
 
     for (var i = 0; i < dicg.length; i++) {
         if (parseInt(dicg[i].dob.split("-")[2]) < 2000) {
